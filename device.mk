6cafefe0627eb4b8084f84c2179d54260a631185
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-03-09 22:09:16 +0700

    D1C: Build QTI Framework detect libs
    Change-Id: Idb5e8f1032c872b76a306369befa36fad15eb02d

--- device.mk
+++ device.mk
@@ -129,6 +129,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
 
+# QMI
+PRODUCT_PACKAGES += \
+    libjson \
+    libqti_vndfwk_detect \
+    libqti_vndfwk_detect.vendor \
+    libvndfwk_detect_jni.qti \
+    libvndfwk_detect_jni.qti.vendor
+
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH)
