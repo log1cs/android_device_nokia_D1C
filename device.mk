be12cc7ee4af2afb74c70f65b55c090708c00895
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-03-09 22:12:15 +0700

    D1C: Build thermal HIDL
    Change-Id: I1851318f4fb5261e8978c9063d5a03ba8c07c34b

--- device.mk
+++ device.mk
@@ -170,6 +170,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
+    thermal.msm8937
+
 # VNDK
 PRODUCT_PACKAGES += \
     libstdc++.vendor
