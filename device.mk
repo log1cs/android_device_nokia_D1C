e2fef3a06606d09749c12fa721e3ec50fc5c5e65
Tuan Anh <tuan73176@gmail.com>
2024-03-09 21:35:10 +0700

    D1C: Add device path to Soong namespaces
    * My build fails without this.
    
    Change-Id: Iff8564ea282bb043cd91999cbef9f59fc423ad67

--- device.mk
+++ device.mk
@@ -4,5 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Soong
+PRODUCT_SOONG_NAMESPACES += \
+    $(DEVICE_PATH)
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/nokia/D1C/D1C-vendor.mk)
