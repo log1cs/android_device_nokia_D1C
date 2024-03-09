dab924b6fef7d6968aad3a2bb89a7b76581458b0
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-03-09 22:07:24 +0700

    D1C: Build OMX stack
    Change-Id: Ie934c35ebdb0a9432d48217e763234dbf5183738

--- device.mk
+++ device.mk
@@ -106,6 +106,21 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
+# OMX
+PRODUCT_PACKAGES += \
+    libc2dcolorconvert \
+    libmm-omxcore \
+    libOmxAacEnc \
+    libOmxAmrEnc \
+    libOmxCore \
+    libOmxEvrcEnc \
+    libOmxG711Enc \
+    libOmxQcelp13Enc \
+    libOmxSwVencHevc \
+    libOmxVdec \
+    libOmxVenc \
+    libstagefrighthw
+
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH)
