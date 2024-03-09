671046621c5c77665d360cd2872c8fdca5b132ce
jmpfbmx <jmpf.bmx@gmail.com>
2024-03-09 20:18:59 +0700

    D1C: Add arch, bootloader and platform flags
    Change-Id: Ie27ff58c05969e6ef0453412a100b10938dbd634
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,5 +4,26 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a53
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := msm8937
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_BOARD_PLATFORM := msm8937
+
 # Kernel
 TARGET_KERNEL_CONFIG := lineageos_D1C_defconfig
