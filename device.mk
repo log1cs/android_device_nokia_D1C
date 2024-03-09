b2161b20107e8c5c4e48f01b60f7d295422f5a19
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-03-09 21:47:30 +0700

    D1C: Build audio stack
    Change-Id: If5cfae4a40883fe551b1ed72d51e36019c477eb0

--- device.mk
+++ device.mk
@@ -4,6 +4,34 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Audio
+PRODUCT_PACKAGES += \
+    android.hardware.audio@4.0-impl \
+    android.hardware.audio.effect@4.0-impl \
+    android.hardware.audio.service \
+    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.soundtrigger@2.1-impl
+
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    audio.bluetooth.default \
+    audio.primary.msm8937 \
+    audio.r_submix.default \
+    audio.usb.default
+
+PRODUCT_PACKAGES += \
+    liba2dpoffload \
+    libaudioroute \
+    libaacwrapper \
+    libaudio-resampler \
+    libaudiopreprocessing \
+    libqcomvisualizer \
+    libqcomvoiceprocessing \
+    libqcompostprocbundle \
+    libtinycompress \
+    libvolumelistener \
+    tinymix
+
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH)
